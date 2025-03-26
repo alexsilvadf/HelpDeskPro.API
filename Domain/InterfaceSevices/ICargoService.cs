77f6e49fad0612bc979a257e12340f23d517ec04
alexu <alex.unapel@gmail.com>
2025-03-25 22:50:41 -0300

    criação as interfaces

---
+++ Domain/InterfaceSevices/ICargoService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.InterfaceSevices
+{
+    public interface ICargoService : ICargo
+    {
+    }
+}
---
+++ Domain/Interfaces/IGenerics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IGenerics<T> where T : class
+    {
+        Task Add(T obj);
+        Task Update (T obj);
+        Task Delete (T obj);
+        Task<T> GetEntityById(int id);
+        Task<List<T>> List();
+    }
+}
