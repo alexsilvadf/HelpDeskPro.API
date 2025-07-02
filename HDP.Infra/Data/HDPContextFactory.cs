58da8c9668144d4dea5e1712c067102cb8a660ec
alexu <alex.unapel@gmail.com>
2025-07-01 23:02:07 -0300

    criação da estrutura de arquivos, entidade de categorias e a migration inicial

---
+++ HDP.Infra/Data/HDPContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDP.Infra.Data
+{
+    public class HDPContextFactory : IDesignTimeDbContextFactory<HDPContext>
+    {
+        public HDPContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<HDPContext>();
+            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS; Database=HelpDeskPro;User Id=sa;Password=123456789; Integrated Security=False");
+
+            return new HDPContext(optionsBuilder.Options);
+        }
+    }
+}
